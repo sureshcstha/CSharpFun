ae57c85f355d4481e3f06b832318b5a602d94997
Suresh Shrestha <sureshcstha@gmail.com>
2024-06-21 17:01:25 -0600

    Serialize and Deserialize

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -13,7 +13,7 @@ namespace CSharpFun.Models
         public int? CPUCores { get; set; } = 0;
         public bool HasWifi { get; set; }
         public bool HasLTE { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
         public decimal Price { get; set; }
         public string VideoCard { get; set; } = "";
 
--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@ using CSharpFun.Models;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.Data;
+using System.Text.Json;
 
 namespace CSharpFun
 {
@@ -42,7 +45,7 @@ namespace CSharpFun
 
 
 
-            string sql = @"INSERT INTO TutorialAppSchema.Computer (
+/*            string sql = @"INSERT INTO TutorialAppSchema.Computer (
                 Motherboard,
                 HasWifi,
                 HasLTE,
@@ -55,20 +58,86 @@ namespace CSharpFun
                     + "','" + myComputer.ReleaseDate
                     + "','" + myComputer.Price
                     + "','" + myComputer.VideoCard
-            + "')";
+            + "')";*/
 
-            File.WriteAllText("log.txt", "\n" + sql + "\n");
+            //File.WriteAllText("log.txt", "\n" + sql + "\n");
 
-            using StreamWriter openFile = new("log.txt", append: true);
+            //using StreamWriter openFile = new("log.txt", append: true);
 
-            openFile.WriteLine("\n" + sql + "\n");
+            //openFile.WriteLine("\n" + sql + "\n");
 
-            openFile.Close();
+            //openFile.Close();
 
-            string fileText = File.ReadAllText("log.txt");
+            string computerJson = File.ReadAllText("Computers.json");
 
-            Console.WriteLine(fileText);
+            //Console.WriteLine(computerJson);
 
+
+            /* Deserialize and Serialize*/
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+
+            IEnumerable<Computer>? computersNewtonsoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computerJson); // Newtonsoft.Json.JsonConvert
+
+            IEnumerable<Computer>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computerJson); // System.Text.Json.JsonSerializer
+
+
+
+            if (computersNewtonsoft != null ) 
+            {
+                foreach(Computer computer in computersNewtonsoft) 
+                {
+                    //Console.WriteLine(computer.Motherboard);
+                    string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                        Motherboard,
+                        HasWifi,
+                        HasLTE,
+                        ReleaseDate, 
+                        Price, 
+                        VideoCard
+                    ) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
+                            + "','" + computer.HasWifi
+                            + "','" + computer.HasLTE
+                            + "','" + computer.ReleaseDate
+                            + "','" + computer.Price
+                            + "','" + EscapeSingleQuote(computer.VideoCard)
+                    + "')";
+
+                    dapper.ExecuteSql(sql); 
+                }
+            }
+
+
+/*            if (computersSystem != null)
+            {
+                foreach (Computer computer in computersSystem)
+                {
+                    Console.WriteLine(computer.Motherboard);
+                }
+            }*/
+
+            JsonSerializerSettings settings = new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            };
+
+            string computersCopyNewtonsoft = JsonConvert.SerializeObject(computersNewtonsoft, settings);
+            File.WriteAllText("computersCopyNewtonsoft.txt", computersCopyNewtonsoft);
+
+            string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computersSystem);
+            File.WriteAllText("computersCopySystem.txt", computersCopySystem);
+
+        }
+
+        // can't have single ' ; we need to replace with ''
+        static string EscapeSingleQuote(string input)
+        {
+            string output = input.Replace("'", "''");
+            return output;
         }
     }
 }
