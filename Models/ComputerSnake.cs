27d9b615efcbce675677c57b3294ff7d0d1e6d7c
Suresh Shrestha <sureshcstha@gmail.com>
2024-06-24 09:26:09 -0600

    model mapping

---
+++ Models/ComputerSnake.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFun.Models
+{
+    public class ComputerSnake
+    {
+        public int computer_id { get; set; }
+        public string motherboard { get; set; } = "";
+        public int? cpu_cores { get; set; } = 0;
+        public bool has_wifi { get; set; }
+        public bool has_lte { get; set; }
+        public DateTime? release_date { get; set; }
+        public decimal price { get; set; }
+        public string video_card { get; set; } = "";
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using AutoMapper;
 using CSharpFun.Data;
 using CSharpFun.Models;
 using Dapper;
@@ -69,6 +70,40 @@ namespace CSharpFun
             //openFile.Close();
 
             string computerJson = File.ReadAllText("Computers.json");
+/*            string computerJson = File.ReadAllText("ComputersSnake.json");
+
+            Mapper mapper = new Mapper(new MapperConfiguration((cfg) =>
+            {
+                cfg.CreateMap<ComputerSnake, Computer>()
+                    .ForMember(destination => destination.ComputerId, options =>
+                        options.MapFrom(source => source.computer_id))
+                    .ForMember(destination => destination.CPUCores, options =>
+                        options.MapFrom(source => source.cpu_cores))
+                    .ForMember(destination => destination.HasLTE, options =>
+                        options.MapFrom(source => source.has_lte))
+                    .ForMember(destination => destination.HasWifi, options =>
+                        options.MapFrom(source => source.has_wifi))
+                    .ForMember(destination => destination.Motherboard, options =>
+                        options.MapFrom(source => source.motherboard))
+                    .ForMember(destination => destination.VideoCard, options =>
+                        options.MapFrom(source => source.video_card))
+                    .ForMember(destination => destination.ReleaseDate, options =>
+                        options.MapFrom(source => source.release_date))
+                    .ForMember(destination => destination.Price, options =>
+                        options.MapFrom(source => source.price));
+            }));
+
+            IEnumerable<ComputerSnake>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<ComputerSnake>>(computerJson);
+
+            if (computersSystem != null)
+            {
+                IEnumerable<Computer> computerResult = mapper.Map<IEnumerable<Computer>>(computersSystem);
+
+                foreach (Computer computer in computerResult)
+                {
+                    Console.WriteLine(computer.Motherboard);
+                }
+            }*/
 
             //Console.WriteLine(computerJson);
 
